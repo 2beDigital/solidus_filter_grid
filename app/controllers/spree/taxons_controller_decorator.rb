bd349b6193a3b176d0532f3fbdc99dcf2c8df4a2
Noel <noel@2bedigital.com>
2017-11-10 15:22:37 +0000

    update version

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::TaxonsController.class_eval do
 
   def show
-    @taxon = Spree::Taxon.find_by_permalink!(params[:id])
+    @taxon = Spree::Taxon.find_by!(permalink: params[:id])
     return unless @taxon
 
     @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
